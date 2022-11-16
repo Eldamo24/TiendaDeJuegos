5f7f21d8d4bc23a5cb5140fcc06d1a9836f63fb9
Damian Lobos <damianlobos24@gmail.com>
2022-11-15 23:43:35 -0300

    creacion de los modelos para la pagina

--- TiendaDeJuegos/Tienda/models.py
+++ TiendaDeJuegos/Tienda/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    email = models.EmailField()
+    nombre_usuario = models.CharField(max_length=50)
+    contrasenia = models.CharField(max_length=50)
+
+class Juego(models.Model):
+    nombre_juego = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    sinopsis = models.CharField(max_length = 100)
+
+class Consola(models.Model):
+    nombre_consola = models.CharField(max_length = 50)
+    precio = models.IntegerField()
