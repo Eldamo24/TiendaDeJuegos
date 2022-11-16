1f66cf2fbbfe2c39462b23fcdc88f0b4fe277f6b
Damian Lobos <damianlobos24@gmail.com>
2022-11-15 23:33:49 -0300

    añadimos plantilla base para la web

--- TiendaDeJuegos/Tienda/urls.py
+++ TiendaDeJuegos/Tienda/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from Tienda.views import *
 
 urlpatterns = [
-
-
+    path("inicio/", vista_inicio, name = "Tienda-inicio")
 ]
--- TiendaDeJuegos/Tienda/views.py
+++ TiendaDeJuegos/Tienda/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def vista_inicio(request):
+    return render(request, "Tienda/base.html")
